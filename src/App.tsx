2fc80c26ebc11ea4a417bd65a6957266b866c07f
Everton <evertondrai@hotmail.com>
2022-09-12 11:58:33 -0300

    finalizando o home iniciando o about

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./styles/home.css"
 import { InstagramLogo, GithubLogo, LinkedinLogo, MediumLogo } from "phosphor-react";
-import userImg from "./styles/image/user.jpg"
 
 function App() {
   return (
@@ -23,25 +22,48 @@ function App() {
       </menu>
 
       <div className="container">
-        <h3>Hello I'm</h3>
-        <h1>Gustavo Delfim</h1>
-        <h2>Developer Engineer</h2>
-        <div className="icons">
-          <a href="https://github.com/GustavoDelfim" target="_blank" >
-            <GithubLogo size={22} />
-          </a>
-          <a href="https://www.linkedin.com/in/gustavodelfim/" target="_blank">
-            <LinkedinLogo size={22} />
-          </a>
-          <a href="https://medium.com/@gusttavodelfim" target="_blank">
-            <MediumLogo size={22} />
-          </a>
-          <a href="https://www.instagram.com/gustavo.delfim/" target="_blank">
-            <InstagramLogo size={22}  />
-          </a>
+        <div className="containerPai">
+          <div>
+            <h3>Hello I'm</h3>
+            <h1>Gustavo Delfim</h1>
+            <h2>Developer Engineer</h2>
+            <div className="icons">
+              <a href="https://github.com/GustavoDelfim" target="_blank" >
+                <GithubLogo size={22} />
+              </a>
+              <a href="https://www.linkedin.com/in/gustavodelfim/" target="_blank">
+                <LinkedinLogo size={22} />
+              </a>
+              <a href="https://medium.com/@gusttavodelfim" target="_blank">
+                <MediumLogo size={22} />
+              </a>
+              <a href="https://www.instagram.com/gustavo.delfim/" target="_blank">
+                <InstagramLogo size={22}  />
+              </a>
+            </div>
+          </div>
+          <div>
+            <div className="imgUser">
+              <img src="/public/image/user.jpg" alt="user image" />
+            </div>
+          </div>
         </div>
-        <div className="imgUser">
-          <img src={userImg} alt="user image" />
+      </div>
+
+      <div className="about">
+        <div>
+          <div className="imageAbout">
+            <img src="/public/image/about.png" alt="About image" />
+          </div>
+          <div className="content">
+            <h1>About Me</h1>
+            <h4>Hello, I'm Gustavo, Web-developer . . . .. . . .  . . .. . </h4>
+            <div>
+              <p>JavaScript</p>
+              <p>React</p>
+              <p>Node.Js</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
